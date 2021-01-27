ecb49dabb12b5c388f846005579a85da35f73931
110694989AW <110694989@ea.edin.sch.uk>
2021-01-27 11:46:55 +0000

    Wrote the program

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  int width = Keyboard.getInt("what is the width of the area in slabs?");
+  int length = Keyboard.getInt("what is the length of the area in slabs?");
+  int area = width * length;
+  double cost = Keyboard.getReal("how much does a slab cost?");
+  double totalCost = cost * area;
+  String output = "the total cost is £" + totalCost;
+  Screen.display(output, "overall cost");
   }
 }
